0650ee294172fbc9617af538ed05db2ea3d20c59
AndriiSyniuchenko <andriisyniuchenko@gmail.com>
2026-03-06 12:29:54 -0800

    Add user schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    role: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(UserBase):
+    id: int
+    is_active: bool
+
+    class Config:
+        from_attributes = True
